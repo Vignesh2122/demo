897a5bb38b01c201eff65a624f3d94a20a89d14f
Vignesh.S <86952970+Vignesh2122@users.noreply.github.com>
2025-07-10 14:35:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,3 +5,8 @@ const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => res.send('Hello from Docker and ECR!'));
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+// These are FAKE AWS credentials for TruffleHog testing
+const AWS_ACCESS_KEY_ID = 'AKIA1234567890FAKE';
+const AWS_SECRET_ACCESS_KEY = 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY';
+
